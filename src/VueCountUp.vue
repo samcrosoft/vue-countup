dd0101b53064b1c400fc220406561bf529335f9c
Adebola Samuel Olowofela <samuel_olowofela@yahoo.com>
2017-02-14 15:21:33 +0000

    made changes to add watchers to countdown values

--- src/VueCountUp.vue
+++ src/VueCountUp.vue
@@ -6,7 +6,9 @@
 <script>
     export default{
         data(){
-            return {}
+            return {
+                _iso: null
+            }
         },
         props: {
             start: {
@@ -43,16 +45,48 @@
                 return this.duration;
             }
         },
+        watch: {
+            endValue(val, oldVal){
+                this.reInitiateCounter();
+            },
+            startValue(val, oldVal){
+                this.reInitiateCounter();
+            },
+            decimalValue(val, oldVal){
+                this.reInitiateCounter();
+            },
+            durationValue(val, oldVal){
+                this.reInitiateCounter();
+            },
+        },
         ready(){
-
             // instantiate the countUp for the element
-            let oCountUp = new CountUp(this.$el, this.startValue,
-                    this.endValue,
-                    this.decimalValue,
-                    this.durationValue,
-                    this.options
-            );
-            oCountUp.start();
+            if(!this._iso){
+                this.initiateCount();
+            }
+        },
+        methods:{
+            initiateCount(){
+                this._iso = new CountUp(this.$el, this.startValue,
+                        this.endValue,
+                        this.decimalValue,
+                        this.durationValue,
+                        this.options
+                );
+                this._iso.start();
+            },
+            clearCountHolder(){
+                this._iso = null;
+            },
+            reInitiateCounter(){
+                // make sure that counter has been instantiated already!
+                if(this._iso){
+                    // clear out the counter
+                    this.clearCountHolder();
+                    // initiate the chart
+                    this.initiateCount();
+                }
+            }
         }
     }
 </script>
